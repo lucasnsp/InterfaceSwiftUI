c88433d6c9930be3fcdd1dd121b0b10f1c6ecd8a
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-12 20:00:24 -0300

    Add files via upload

---
+++ Interface/Interface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Interface
+//
+//  Created by Lucas Neves dos santos pompeu on 12/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Interface/Interface/InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceApp.swift
+//  Interface
+//
+//  Created by Lucas Neves dos santos pompeu on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
